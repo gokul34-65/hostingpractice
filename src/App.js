d209718da9c8603fda9470dbc09e5b969eb9ce5f
gokul34-65 <gokulkarunaharan41@gmail.com>
2025-11-02 07:51:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey there i am gokul 
         </p>
         <a
           className="App-link"
